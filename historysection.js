267a6b24cf1fc9e1341c8cd598dd5bf006e7e962
Arpan-collab <deyr00081@gmail.com>
2024-10-04 07:35:23 +0600

    Update historysection.js

--- historysection.js
+++ historysection.js
@@ -1,4 +1,7 @@
-const BtnHome = document.getElementById('btn-home')
-BtnHome.addEventListener('click',function(){
-    window.location.href = "./index.html"
-})
+// Blog button click event
+let BtnBlogHome = document.getElementById('btn-blog-home');
+BtnBlogHome.addEventListener('click', function () {
+    window.location.href = "/index.html"; 
+});
+
+
